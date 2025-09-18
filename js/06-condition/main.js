a4bb19624549ff3a919c1c0b356939df1fa3ab22
stanstanstanlee <stanstanstanlee@gmail.com>
2025-09-18 11:03:12 +0900

    js-condition

--- js/06-condition/main.js
+++ js/06-condition/main.js
@@ -21,3 +21,109 @@ if (조건식1) {
 } 
 
  */
+
+if (5 > 10) {
+  console.log("5는 10보다 크다");
+} else if (5 > 7) {
+  console.log("5는 7보다 크다");
+} else if (5 > 5) {
+  console.log("5는 5보다 크다");
+} else if (5 > 3) {
+  console.log("5는 3보다 크다");
+}
+
+/* if ~ else if ~ else 조건문 기본 구조
+if(조건식2) {
+} else if(조건식2) {
+} else if (조건식3) {
+} else {
+  else는 조건식이 없다.
+  대신 위의 모든 조건식이 거짓(false)이면 실행   
+  else 아래에는 또 다른 else if나 else가 존재 할 수 없다
+} 
+---
+if(조건식) {
+} else {
+  조건식이 거짓이면 실행  
+}
+*/
+
+// 변수 number를 선언 하고, 숫자 1을 할당
+let number = 1;
+
+// number 가 0보다 크면 "양수" 를 콘솔에 출력
+// number 가 0보다 큰게 아니라면 "음수"를 콘솔에 출력
+// if ~ else 문 구현
+if (number > 0) {
+  console.log("양수");
+} else console.log("음수");
+
+let answr = number > 0 ? "양수" : "음수";
+console.log(`answer : ${answr}`);
+
+console.log(`answer : ${number > 0 ? "양수" : "음수"}`);
+result = number > 0 ? "양수" : "음수";
+
+/*
+만약에 변수 number2 가 0 보다 크면, "양수" 출력
+그런데 만약에 변수 number2가 0 보다 작으면, "음수" 출력
+그런데 모두 아니라면, "0" 출력
+
+조건문 코드 작성
+ */
+let number2;
+if (number2 > 0) {
+  console.log("양수");
+} else if (number2 < 0) {
+  console.log("음수");
+} else console.log("0");
+
+console.log(number2 > 0 ? "양수" : number < 0 ? "음수" : "0");
+
+//중첩 조건문
+//조건문 내부에 조건문을 중첩하는 조건문
+
+//1. 변수 age가 20 이상이라면
+//1-1. 변수 isStudent가 true라면
+//1-2. 변수 isStudent가 true가 아니라면
+
+// 2. 변수 age가 20 이상이 아니라면
+// 2-1. 변수 isStudent가 true라면
+// 2-2. 변수 isStudent가 true가 아니라면
+let age = 25;
+let isStudent = true;
+
+if (age >= 20) {
+  if (isStudent === true) {
+    console.log("성인 학생입니다.");
+  } else {
+    console.log("성인입니다.");
+  }
+} else {
+  if (isStudent === true) {
+    console.log("미성년 학생입니다.");
+  } else {
+    console.log("미성년입니다.");
+  }
+}
+// 변수 score를 선언하고, 숫자 75를 할당한다
+// 만약 변수 socre가 90 이상이라면
+// 그런데 만약 변수 score가 90 미만 그리고(&&), 80 이상이라면
+// 그런데 만약 변수 score가 80 미만 그리고(&&), 70 이상이라면
+// 그런데 만약 변수 score가 70 미만 그리고(&&), 60 이상이라면
+// 그런데 모두 아니라면
+
+let score = 75;
+if (score >= 90) {
+} else if (score < 90 && score >= 80) {
+} else if (score < 80 && score >= 70) {
+} else if (score < 70 && score >= 60) {
+} else {
+}
+
+if (score >= 90) {
+} else if (score >= 80) {
+} else if (score >= 70) {
+} else if (score >= 60) {
+} else {
+}
