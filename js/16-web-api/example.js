9c3b8116b47a708c1e56a85fd3b890dc0c13530d
stanstanstanlee <stanstanstanlee@gmail.com>
2025-09-23 16:05:47 +0900

    axios

--- js/16-web-api/example.js
+++ js/16-web-api/example.js
@@ -22,10 +22,9 @@ async function getAllProducts() {
 }
 // getAllProducts();
 
-// const : 변수를 재할당할 수 없도록 선언 (상수처럼 쓰이지만 객체/배열은 내용 변경 가능)
+// getProductById 라는 변수 (저장공간)을 선언.
 // async : 비동기 함수임을 표시 → 함수 안에서 await 사용 가능
-// (n) : 함수의 매개변수 (parameter). 호출할 때 전달받은 값이 n에 들어옴
-// => : 화살표 함수 문법. function 키워드 대신 사용
+// 함수의 매개변수 (parameter)로 n을 선언. 함수를 호출할 때 전달받은 값이 n에 들어옴
 const getProductById = async (n) => {
   // axios.get(...) : axios 라이브러리의 GET 요청 메서드를 사용 해서
   // await : Promise(비동기 작업)이 끝날 때까지 기다렸다가 결과를 반환 후 res 에 저장
@@ -33,6 +32,7 @@ const getProductById = async (n) => {
   // res 객체 안의 "data" 프로퍼티를 꺼내서 data에 저장
   const data = res["data"];
   console.log(data); //출력 : res : 전체 응답 객체 출력 (status, headers, data 등 포함)
+  //console.log(Object.keys(data));
 };
 
 getProductById(20);
