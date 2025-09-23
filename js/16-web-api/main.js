2078c97b5b7166da0d3f2ee0951a1363a079c0b7
stanstanstanlee <stanstanstanlee@gmail.com>
2025-09-23 15:22:30 +0900

    axios

---
+++ js/16-web-api/main.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+//async / await
+async function connectTest() {
+  // axios.get(url): 데이터 요청에 대한 응답을 반환
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  console.log(response);
+  console.table(Object.entries(response));
+}
+connectTest();
---
+++ note/js_note/fetch.md
@@ -0,0 +1,11 @@
+# fetch API
+
+- HTTP 요청 및 응답 비동기 처리
+- Promise 기반
+  - promise-then, async-await가 가능 하다
+- 웹 브라우저가 제공하는 Web API
+
+# JavaScript axios
+
+- axios는 패키지다
+- 비동기 HTTP 요청 & 응답 처리를 위한 JavaScript 라이브러리
