ba92cf3cca7f648155b391c8e0fca7d84d437ab5
stanstanstanlee <stanstanstanlee@gmail.com>
2025-09-26 14:03:03 +0900

    nullish

---
+++ js/23-modern/optional.js
@@ -0,0 +1,18 @@
+//욥셔널 채이닝
+const user = {
+  name: "홍길동",
+  address: {
+    city: "서울",
+  },
+};
+//["속성명"]
+//.속성명
+// 옵셔널 체이닝 -> ?.속성명
+
+console.log(user?.address); //{ city: '서울'}
+console.log(user?.address?.city); // 서울
+console.log(user?.address?.country); // undefined
+
+console.log(user?.contact?.mail); //undefined
+console.log(user?.contact?.phone); //undefined
+console.log(user?.contact); //undefined
