88dd64c160aeb82dc5bae4290da118b82f20e666
stanstanstanlee <stanstanstanlee@gmail.com>
2025-09-26 16:45:09 +0900

    components add

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Rule from "./components/JSX/Rule";
 import Interpolation from "./components/JSX/Interpolation";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 export default function App() {
   return (
     <div>
       {/* <Rule></Rule> */}
       <Interpolation></Interpolation>
+      <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
