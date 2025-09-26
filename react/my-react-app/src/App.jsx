d8652711851478ae982ecc90c13afc622f15ebe5
stanstanstanlee <stanstanstanlee@gmail.com>
2025-09-26 16:36:00 +0900

    react rule

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
+import Rule from "./components/JSX/Rule";
+import Interpolation from "./components/JSX/Interpolation";
 
-// export default : 모듈 기본 내보내기
-// function : 함수
-// return : 요소(Element)를 반환
-
-// 파일 당 하나 존재하는 함수는 컴포넌트를 뜻하는데
-// 컴포넌트 함수는 HTML 요소(element)를 반환
 export default function App() {
-  //
-  //
-  //
-  return <div className="font-bold text-blue-900">App</div>;
+  return (
+    <div>
+      {/* <Rule></Rule> */}
+      <Interpolation></Interpolation>
+    </div>
+  );
 }
