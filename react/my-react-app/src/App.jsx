ec16370d8a7998a11d627ac6118779045e9ef0ce
stanstanstanlee <stanstanstanlee@gmail.com>
2025-09-26 17:23:29 +0900

    jsx component

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -3,12 +3,16 @@ import Rule from "./components/JSX/Rule";
 import Interpolation from "./components/JSX/Interpolation";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import MyButton from "./components/Component/MyButton";
+import MyList from "./components/Component/MyList";
 export default function App() {
   return (
     <div>
       {/* <Rule></Rule> */}
-      <Interpolation></Interpolation>
       <Header></Header>
+      <Interpolation />
+      <MyList />
+      <MyButton />
       <Footer></Footer>
     </div>
   );
--- react/my-react-app/src/components/Header.jsx
+++ react/my-react-app/src/components/Header.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Interpolation from "./JSX/Interpolation";
 
+// App 컴포넌트는 Header 컴포넌트와 Interpolation 컴포넌트를 포함한다
+// App 컴포넌트 Header 컴포넌트와 Interpolation 컴포넌트가 중첩됐다.
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      Header
+      <Interpolation></Interpolation>
+    </div>
+  );
 }
