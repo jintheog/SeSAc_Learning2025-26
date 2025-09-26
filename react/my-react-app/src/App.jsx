f68d569b12e1a8cc8eac31b4914d816fcf979298
stanstanstanlee <stanstanstanlee@gmail.com>
2025-09-26 16:01:12 +0900

    react styles

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+// export default : 모듈 기본 내보내기
+// function : 함수
+// return : 요소(Element)를 반환
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// 파일 당 하나 존재하는 함수는 컴포넌트를 뜻하는데
+// 컴포넌트 함수는 HTML 요소(element)를 반환
+export default function App() {
+  //
+  //
+  //
+  return <div className="font-bold text-blue-900">App</div>;
 }
-
-export default App
