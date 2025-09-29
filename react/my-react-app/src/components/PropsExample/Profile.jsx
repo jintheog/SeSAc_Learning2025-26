381e2b7db76ef20948e745b4b924d92d38eb75ad
stanstanstanlee <stanstanstanlee@gmail.com>
2025-09-29 16:03:39 +0900

    props

--- react/my-react-app/src/components/PropsExample/Profile.jsx
+++ react/my-react-app/src/components/PropsExample/Profile.jsx
@@ -4,10 +4,14 @@ export default function Profile(props) {
   console.log("Profile props:", props);
   console.log("isAdmin value:", props.isAdmin, "type:", typeof props.isAdmin);
 
+  // user 객체가 있으면 user 객체의 속성을 사용하고, 없으면 개별 props를 사용
+  const name = props.user ? props.user.name : props.name;
+  const age = props.user ? props.user.age : props.age;
+  const isAdmin = props.user ? props.user.isAdmin : props.isAdmin;
+
   return (
     <div>
-      저는 {props.name}이고, {props.age}세 이며, 관리자 여부는{" "}
-      {String(props.isAdmin)} 입니다.
+      저는 {name}이고, {age}세 이며, 관리자 여부는 {String(isAdmin)} 입니다.
     </div>
   );
 }
--- react/my-react-app/src/components/PropsExample/ProfileContainer.jsx
+++ react/my-react-app/src/components/PropsExample/ProfileContainer.jsx
@@ -5,6 +5,7 @@ export default function ProfileContainer() {
     <div>
       <Profile name="현우" age={22} isAdmin={true} />
       <Profile name="수진" age={21} isAdmin={false} />
+      <Profile user={{ name: "완득", age: 22, isAdmin: true }} />
     </div>
   );
 }
