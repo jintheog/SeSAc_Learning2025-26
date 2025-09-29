efc798da17468aa502b3acd3ff80cb42e45b6b13
stanstanstanlee <stanstanstanlee@gmail.com>
2025-09-29 10:21:00 +0900

    react

---
+++ react/test-react-app/src/components/Main.jsx
@@ -0,0 +1,10 @@
+// 파일의 첫 글자를 대문자로 작성
+// 함수명 (컴포넌트명)도 첫 글자를 대문자로 작성
+
+// Main 컴포넌트를 App 컴포넌트 에서 불러와서
+// App 컴포넌트 내부에 포함시키기
+import React from "react";
+
+export default function Main() {
+  return <div>Main</div>;
+}
