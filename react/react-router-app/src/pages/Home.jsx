ed90dd13f38d03f2bb82d29ff23b40200dc4ea69
stanstanstanlee <stanstanstanlee@gmail.com>
2025-10-13 10:35:29 +0900

    router

--- react/react-router-app/src/pages/Home.jsx
+++ react/react-router-app/src/pages/Home.jsx
@@ -1,5 +1,17 @@
-import React from "react";
-
+//a태그를 대체하는 Link 컴포넌트
+import { Link } from "react-router-dom";
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="font-bold text-4xl">홈</h1>
+      {/** to 속성 : 어떤 주소로 이동할것인지? */}
+      <Link to="/">홈</Link>
+      <br></br>
+      <Link to="/about">소개</Link>
+      <br></br>
+      <a href="https://www.naver.com">외부 서비스 이동</a>
+      <br />
+      <Link to="/profile">사용자 정보</Link>
+    </div>
+  );
 }
--- react/react-router-app/src/router/index.js
+++ react/react-router-app/src/router/index.js
@@ -1,7 +1,7 @@
 //페이지 컴포넌트 불러오기
 import Home from "../pages/Home";
 import About from "../pages/About";
-
+import Profile from "../pages/Profile";
 // React Royter의 createBrowserRouter  불러오기
 // createBrowserRouter 함수
 // 라우터 설정을 생성하는 함수
@@ -18,6 +18,12 @@ const router = createBrowserRouter([
   {
     path: "/about",
     Component: About,
+  },
+  // "/profile"로 접근하면
+  //Profile 컴포넌트를 표시하는 속성 작성
+  {
+    path: "/profile",
+    Component: Profile,
   },
 ]);
 
