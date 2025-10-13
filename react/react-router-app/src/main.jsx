236ddb69246e2d0e7f2f1cc8bbc363f4348cbf37
stanstanstanlee <stanstanstanlee@gmail.com>
2025-10-13 09:39:14 +0900

    router

--- react/react-router-app/src/main.jsx
+++ react/react-router-app/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import router from "./router";
 
-createRoot(document.getElementById('root')).render(
+//라우터 설정을 애플리케이션에 적용할 컴포넌트(Provider)
+import { RouterProvider } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/** RouterProvider 배치 */}
+    {/** router 속성 : 어떤 라우터 설정을 사용할 것이냐? */}
+    <RouterProvider router={router} />
+  </StrictMode>
+);
